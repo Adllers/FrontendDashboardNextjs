1816473840b821e6d54bb3ef449ecc63cc150263
adller <adller.eel.ufsc@gmail.com>
2022-01-27 13:18:02 -0300

    continuando com react-query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,26 +6,14 @@ import { SideBar } from "../../components/Sidebar";
 import Link from 'next/link';
 import { useEffect } from "react";
 import { useQuery } from 'react-query';
+import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/useUsers";
 
 
 export default function UserList() {
-
-    const { data, isLoading, error } = useQuery('users', async () => {
-       const response = await fetch('http://localhost:3000/api/users');
-       const data = await response.json();
-
-       const users = data.users.map(user => {
-           return {
-               id: user.id,
-               name: user.name,
-               email: user.email,
-               createdAt: new Date(user.created_at).toLocaleDateString('pt-BR', { day: '2-digit', month: 'long', year: 'numeric'}),
-           }
-       })
-
-       return users;
-    }, { staleTime: 1000 * 5 });
-    //staleTime -> em 5 segundos não vai haver recarregamento página
+    //fetching acontece quando troca-se de abas no navegador, navega-se por dentro da aplicação
+    //loading acontece ao dar f5 na pagina
+    const { data, isLoading, isFetching, error } = useUsers();
 
 
     const isWideVersion = useBreakpointValue({
@@ -44,7 +32,12 @@ export default function UserList() {
 
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+                        <Heading size="lg" fontWeight="normal">
+                            Usuários
+                            { // na atualização dos dados
+                            }
+                            { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4"/> }  
+                        </Heading>
 
                         <Link href="/users/create" passHref>
 
---
+++ src/services/hooks/useUsers.ts
@@ -0,0 +1,38 @@
+import { useQuery } from 'react-query';
+import { api } from '../api';
+
+
+type User = {
+    id: string;
+    name: string;
+    email: string;
+    createdAt: string;
+}
+
+export async function getUsers(): Promise<User[]>{
+   
+    const { data } = await api.get('users');
+        
+    const users = data.users.map(user => {
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            createdAt: new Date(user.created_at).toLocaleDateString('pt-BR', { day: '2-digit', month: 'long', year: 'numeric'}),
+        }
+    })
+
+    return users;
+}
+
+//fetching acontece quando troca-se de abas no navegador, navega-se por dentro da aplicação
+//loading acontece ao dar f5 na pagina
+export function useUsers() {
+    return useQuery<User[]>('users', getUsers, {
+        staleTime: 1000 * 5,
+        //staleTime -> em 5 segundos não vai haver recarregamento página
+    })
+}
+
+    
+    
